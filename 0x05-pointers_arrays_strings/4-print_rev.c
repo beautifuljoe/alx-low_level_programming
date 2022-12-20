dab5f7121fd9d0c28dc34ff953f2c988a2f88c14
Trust Unutakunu <t2rd2050@outlook.com>
2022-12-19 19:14:17 -0800

    4-print_rev.c

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * _print rev - this characters in reverse.
+ * @s: the string to printed
+ */
+
+void print_rev(char *s);
+{
+	int a, b;
+
+	b = 0;
+
+	while (s[b] != '\0')
+		b++
+
+	for (a = b - 1; a >= 0; a--)
+	{
+		_putchar(s[i]);
+	}
+	_putchar('\n')
+
+

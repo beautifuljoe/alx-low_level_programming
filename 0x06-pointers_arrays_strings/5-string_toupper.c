d6add0a04df13a2ba0c2e5d78c1e1edad590f168
Trust Unutakunu <t2rd2050@outlook.com>
2022-12-22 01:41:07 -0800

    5-string_toupper.c

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -6,12 +6,13 @@ nclude "main.h"
  */
 char *string_toupper(char *n)
 {
-	int i;
+	int i = 0;
 
-	for (i = 0; n[i] != '\0'; i++)
+	while (n[i])
 	{
-		if (n[i] >= 'a' && n[i] <= 'z')
+		if (n[i]  >= 97 && n[i] <= 122)
 			n[i] = n[i] - 32;
+		i++;
 	}
 	return (n);
 }
